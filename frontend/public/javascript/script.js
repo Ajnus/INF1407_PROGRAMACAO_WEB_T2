3cd958c8bd0f93a26579dd231f45c923f6ef7b7b
MiguelGarcia <mpgarcia.br@gmail.com>
2023-11-17 12:39:23 -0300

    mudanças para pegar script

---
+++ frontend/public/javascript/script.js
@@ -0,0 +1,26 @@
+"use strict";
+onload = function () {
+    exibeListaPubs(); // exibe lista de publicacoes ao carregar a página
+};
+function exibeListaPubs() {
+    fetch(backendAddress + "carros/lista/")
+        .then(response => response.json())
+        .then(pubs => {
+        let campos = ['titulo'];
+        let tbody = document.getElementById('idtbody');
+        tbody.innerHTML = "";
+        for (let pub of pubs) {
+            let tr = document.createElement('tr');
+            for (let i = 0; i < campos.length; i++) {
+                let td = document.createElement('td');
+                let texto = document.createTextNode(pub[pubs[i]]);
+                td.appendChild(texto);
+                tr.appendChild(td);
+            }
+            tbody.appendChild(tr);
+        }
+    })
+        .catch(error => {
+        console.error("Erro:", error);
+    });
+}
