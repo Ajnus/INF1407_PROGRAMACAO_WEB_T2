c858ba26e40bc36a2e7f7a6f45ac2ab13d8ef7a6
MiguelGarcia <mpgarcia.br@gmail.com>
2023-11-24 22:41:29 -0300

    visualisar comentarios front

--- frontend/typescript/visualizaPub.ts
+++ frontend/typescript/visualizaPub.ts
@@ -26,4 +26,81 @@ onload = () => {
     } else {
          idPlace.innerHTML = 'URL mal formada: ' + window.location;
     }
+    (document.getElementById('insere') as HTMLButtonElement)
+        .addEventListener('click', evento => { location.href = 'insereCom.html' });
+    exibeListaComs(id); // exibe lista de publicacoes ao carregar a página
+
+
+
+}
+
+function exibeListaComs(id: string | null) {
+
+
+    const token = localStorage.getItem('token'); // Recupera o token de autenticação
+
+    // Check if token is not null before using it in the headers
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json',
+    };
+
+    if (token !== null) {
+        headers['Authorization'] = token;
+    }   
+    let username = 'visitante';
+
+    fetch(backendAddress + 'accounts/token-auth/', {
+        method: 'GET',
+        headers: headers,
+    })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            const usuario = data;
+            if (usuario.username) {
+                username = usuario.username;
+            }
+    
+            // Now that the first fetch is complete, initiate the second fetch
+            return fetch(backendAddress + 'forum/pub/public/' + id + '/');
+        })
+        .then(response => response.json())
+        .then(pub => {
+            console.log(pub);
+            console.log(username);
+            let coms = pub.comentarios
+            let campos = ['autor_username','texto','deletar']; // Adicionei 'editar' como terceiro campo
+            let tbody = document.getElementById('idtbody') as HTMLTableSectionElement;
+            tbody.innerHTML = "";
+            for (let com of coms) {
+                let tr = document.createElement('tr') as HTMLTableRowElement;
+                for (let i = 0; i < campos.length; i++) {
+                    let td = document.createElement('td') as HTMLTableCellElement;
+                    if (campos[i] === 'deletar'){
+                        if (com['autor_username'] === username){
+                            let href = document.createElement('a') as HTMLAnchorElement;
+                            href.setAttribute('href', 'deleteCom.html?id=' + com['id']);
+                            let texto = document.createTextNode("Apagar") as Text;
+                            href.appendChild(texto);
+                            td.appendChild(href);
+                        }
+                        else{
+                            let texto = document.createTextNode("") as Text;
+                            td.appendChild(texto);
+                        }                               
+                    }
+                else {
+                        let texto = document.createTextNode(com[campos[i]]) as Text;
+                        td.appendChild(texto);
+
+                    }
+                    tr.appendChild(td);
+                }
+                tbody.appendChild(tr);
+            }
+        })
+        .catch(error => {
+            console.error("Erro:", error);
+        });
 }
